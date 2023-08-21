394d4890dbf1a65f0ea4647cd2b8bc33de28f544
EdouardADM <greedoghost911@gmail.co>
2023-08-21 18:25:07 +0200

    affichage

--- fruit_legume/fruitLegume.js
+++ fruit_legume/fruitLegume.js
@@ -1,6 +1,9 @@
-var fruits = [
+let fruits = [
     {fruit: 'poire',  prix: 4.16},   {fruit: 'ananas', prix: 2.99},   {fruit: 'dattes', prix: 6.25},   {fruit: 'orange', prix: 1.50},   {fruit: 'pomme',  prix: 1.79},   {fruit: 'banane', prix: 2.31},   {fruit: 'citron', prix: 3.70},   {fruit: 'raisin', prix: 2.49},   {fruit: 'noix',   prix: 7.80},   {fruit: 'prune',  prix: 4.52},   {fruit: 'peche',  prix: 3.99}
 ];
+
+let commande = [];
+let total = 0;
 
 function init() {
     fruits.sort((a, b) => {
@@ -22,8 +25,14 @@ function init() {
     let elem1 = refElem('liste1');
     let elem2 = refElem('liste2');
 
+    elem1.selectedIndex = -1;
+    elem2.selectedIndex = -1;
+
+
     elem1.addEventListener('change', synchroSelect); 
     elem2.addEventListener('change', synchroSelect);
+
+    tot = document.getElementById('txt').innerHTML = total.toFixed(2)
 }
 
 function synchroSelect(event) {
@@ -36,4 +45,36 @@ function synchroSelect(event) {
     else if (event.target === elem2) {
             elem1.selectedIndex = elem2.selectedIndex
     }
+}
+
+function ajouterFruit(event){
+    event.preventDefault();
+    //recuperer 
+    let elem1 = refElem('liste1');
+
+    let index = elem1.selectedIndex
+
+    let fruit = fruits[index]
+
+    let poid = refElem('poids').value;
+    //ajouter dans le tableau
+    fruit.poid = poid;
+    commande.push(fruit);
+
+    //afficher le tableau plus le total
+    affichage();
+
+    //remttre les champs à zero
+
+}
+
+function affichage () {
+    let lignes = "";
+    for(let i of commande) {
+        let s = '<tr>'
+        let t = i.prix * i.poid
+        s += '<td>' + i.fruit + '</td>' + '<td>' + i.prix + '</td>' + '<td>' + i.poid + '</td>' + '<td>' + t + '</td></tr>';
+        lignes += s;
+    }
+    setElem('aff', lignes)
 }
